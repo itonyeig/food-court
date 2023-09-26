c10766d46a199847b0ded426658924e01649fa45
itonyeig <itonyeig@gmail.com>
2023-09-26 18:06:54 +0100

    set up env file

--- knexfile.ts
+++ knexfile.ts
@@ -2,10 +2,10 @@ const config = {
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
-      user: 'macbook',
-      password: null,
-      database: 'test_db',
+      host: process.env.DB_HOST || '127.0.0.1',
+      user: process.env.DB_USER || 'macbook',
+      password: process.env.DB_PASSWORD || null,
+      database: process.env.DB_NAME || 'test_db',
     },
     migrations: {
       directory: './src/migrations',
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

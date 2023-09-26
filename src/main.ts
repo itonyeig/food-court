6ce1844f3900bcce6859f9c759c5e1fff8c9175b
itonyeig <itonyeig@gmail.com>
2023-09-26 20:11:41 +0100

    moved .env file to the top and added env.example

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
-import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }

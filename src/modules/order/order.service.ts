ae7c8080b4fda450dbcbb5df3efb74777e9e466f
itonyeig <itonyeig@gmail.com>
2023-09-26 07:05:01 +0100

    moving files

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import Order from './model/order.model';
+import Order from './order.model';
 
 @Injectable()
 export class OrderService {
